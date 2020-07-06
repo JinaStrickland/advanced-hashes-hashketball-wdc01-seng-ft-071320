e53d71b12d64bc0c65e488dc36ece20b923e82c8
Jina Strickland <jina30@gmail.com>
2020-07-06 16:56:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -206,7 +206,8 @@ end
 def big_shoe_rebounds
   big_size = 0
   game_hash.each do |location, team_data|
-    team_data[:players].each do |player|
+    team_data[:players].each.value do |player|
+binding.pry
       player[:shoe].each.value do |shoe_size|
 binding.pry
         if shoe_size > big_size
